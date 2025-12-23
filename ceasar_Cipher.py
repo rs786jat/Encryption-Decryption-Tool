b9e584dc89a85885304025889686526a203bacb7
Rajesh jat <143875115+rs786jat@users.noreply.github.com>
2025-12-23 14:48:21 +0530

    Caesar cipher

---
+++ ceasar_Cipher.py
@@ -0,0 +1,41 @@
+
+# Encryption-Decryption-Tool (Caesar Cipher Tool)
+## Project Description
+
+The Caesar cipher is one of the simplest and one of the earliest known encryption techniques. It is a type of substitution cipher that 'shifts' a letter by a fixed amount in the alphabet. This tool allows you to encrypt and decrypt messages using the Caesar cipher.
+
+## Team Member
+
+- Student Name: Bhavya Badaya			Roll No: 23CA2591010
+- Student Name: Rajesh Jat				Roll No: 23CA2591040
+
+
+
+## Features
+
+- Encrypts and decrypts messages using the Caesar cipher.
+- Supports both lowercase and uppercase letters, as well as spaces.
+- Simple and easy to use.
+
+## Installation
+
+No installation is required. 
+Simply clone the repository and run the script using Python.
+
+## Example
+
+* Encryption
+
+Message: Hello World!
+
+Shift: 3
+
+Encrypted Message: Khoor Zruog!
+
+* Decryption
+
+Encrypted Message: Khoor Zruog!
+
+Shift: 3
+
+Decrypted Message: Hello World!
